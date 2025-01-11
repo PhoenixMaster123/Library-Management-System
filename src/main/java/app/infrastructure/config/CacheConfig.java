a6746487b257c0313b25046d63e699eaeed2a039
PhoenixMaster123 <kristianpopov03@yahoo.com>
2025-01-11 12:08:29 +0100

    added CachingConfig

--- src/main/java/app/infrastructure/config/CacheConfig.java
+++ src/main/java/app/infrastructure/config/CacheConfig.java
@@ -1,16 +1,15 @@
 package app.infrastructure.config;
 
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import jakarta.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class CacheConfig {
+
     @Bean
-    public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("books", "authors", "customers", "transactions");
+    public Filter shallowEtagFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 }
